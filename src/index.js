a39df60ee9f314b8473acaff20804c7f31664ffb
Annaliese Mathews <amesibov043@gmail.com>
2023-06-20 21:04:51 -0500

    added CssBaseLine to index.js

--- src/index.js
+++ src/index.js
@@ -3,11 +3,24 @@ import { createRoot } from "react-dom/client"
 import "./index.css"
 import { BrowserRouter } from "react-router-dom"
 import { Craft } from './Craft'
+import { ThemeProvider } from "@mui/material"
+import { createTheme } from "@mui/material"
+import CssBaseline from '@mui/material/CssBaseline';
+
+const theme = createTheme({
+    palette: {
+        mode: 'dark',
+    }
+});
+
 
 const container = document.getElementById("root")
 const root = createRoot(container)
 root.render(
+    <ThemeProvider theme={theme}>
+         <CssBaseline />
     <BrowserRouter>
-        <Craft/>
+        <Craft />
     </BrowserRouter>
+    </ThemeProvider>
 )
