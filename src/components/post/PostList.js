5da59ae88f6811b20f21fce87dc53157541b484a
Annaliese Mathews <amesibov043@gmail.com>
2023-06-27 17:53:20 -0500

    deleted unused imports from PostList

--- src/components/post/PostList.js
+++ src/components/post/PostList.js
@@ -1,27 +1,15 @@
 import React, { useEffect, useState } from "react"
-import { getOtherUserPosts, newComment } from "../../managers/PostManager.js"
+import { getOtherUserPosts } from "../../managers/PostManager.js"
 import { PostDetails } from "./PostDetails.js"
-import { useNavigate } from "react-router-dom"
 import { NewPostForm } from "../form/NewPostForm.js"
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
-import Chip from '@mui/material/Chip';
 import Stack from '@mui/material/Stack';
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
-
 
 
 import "./post.css"
 
 export const PostList = () => {
     const [posts, setPosts] = useState([])
-    const [comment, setComment] = useState('')
-    const navigate = useNavigate()
 
     const refreshPosts = () => {
         getOtherUserPosts().then(data => setPosts(data.reverse()))
@@ -30,18 +18,6 @@ export const PostList = () => {
     useEffect(() => {
         refreshPosts()
     }, [])
-
-    const handleSubmitComment = (postId) => {
-        const commentToSubmit = {
-            "message": comment,
-            "post": postId
-        }
-
-        newComment(commentToSubmit).then(() => getOtherUserPosts()).then((data) => {
-            setComment('')
-            setPosts(data.reverse())
-        })
-    }
 
     return (
         <article>
